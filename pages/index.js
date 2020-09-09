c16e3a490af4a0a89f8b5d52a29a40319538fabe
17gstyron <17gstyron@gmail.com>
2020-09-09 14:31:31 -0500

    Art viewer basic functionality working. Just added pre rendering for images before and after current images

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,175 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Unsplash, { toJson } from "unsplash-js";
+import { useState, useEffect } from "react";
+import config from "../config";
 
-export default function Home() {
+const unsplash = new Unsplash({
+  accessKey: config.unsplashAPI,
+});
+
+export default function Home(props) {
+  const [images, setImages] = useState(props.data || {});
+  const [image, setImage] = useState(
+    "https://wallpaperaccess.com/full/173801.png"
+  );
+  const [imageIndex, setImageIndex] = useState(0);
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  const [searchBarValue, setSearchBarValue] = useState("");
+
+  useEffect(() => {
+    if (images[0]) {
+      setImage(images[imageIndex].urls.raw);
+    }
+    if (typeof window !== "undefined") {
+      // Handler to call on window resize
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      window.addEventListener("resize", handleResize);
+
+      handleResize();
+
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      search();
+    }
+  }
+
+  async function search() {
+    await unsplash.search
+      .photos(searchBarValue, 1, 100, {})
+      .then(toJson)
+      .then((res) => {
+        setImages(res.results);
+        setImage(res.results[0].urls.raw);
+      });
+    setImageIndex(0);
+  }
+
+  function traverseImageArray(direction) {
+    if (
+      (imageIndex === 0 && direction === -1) ||
+      (imageIndex === images.length - 1 && direction === 1)
+    ) {
+      alert("No more that way");
+    } else {
+      setImageIndex(imageIndex + direction);
+      setImage(images[imageIndex + direction].urls.raw);
+    }
+  }
+
+  function loadNextImages(currentIndex) {
+    // TODO render images that are directly before and after the current image being viewed
+    // Return two rendered images
+    if (typeof window !== "undefined") {
+      console.log("rendering...");
+      const indexOfElementsToRender = [currentIndex - 1, currentIndex + 1];
+      const renderedImages = indexOfElementsToRender.forEach((index) => {
+        if (index < 0 || index > images.length - 1) {
+          console.log("nothing to render here");
+        } else {
+          console.log("rendering image...");
+          const img = new Image();
+          img.src = images[index].urls.raw; // by setting an src, you trigger browser download
+
+          img.onload = () => {
+            // when it finishes loading, update the component state
+            console.log(`Succesfully loaded Image at index: ${index}`);
+          };
+        }
+      });
+    }
+  }
+
+  loadNextImages(imageIndex);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div
+      style={{
+        height: windowSize.height,
+        width: windowSize.width,
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ position: "absolute", top: "10%" }}>
+        <div className="form__group field">
+          <input
+            type="input"
+            className="form__field"
+            placeholder="Name"
+            name="name"
+            id="name"
+            value={searchBarValue}
+            onChange={(e) => setSearchBarValue(e.target.value)}
+            style={{ width: 500 }}
+            autoComplete="off"
+            onKeyDown={handleKeyDown}
+          />
+          <label className="form__label">Search</label>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
+      <a
+        href="#"
+        className="previous round"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: 75,
+          width: 75,
+          height: 75,
+          textAlign: "center",
+          lineHeight: 1.1,
+          fontSize: 50,
+        }}
+        onClick={() => {
+          traverseImageArray(-1);
+        }}
+      >
+        &#8249;
+      </a>
+      <a
+        href="#"
+        className="next round"
+        style={{
+          position: "absolute",
+          top: "50%",
+          right: 75,
+          width: 75,
+          height: 75,
+          textAlign: "center",
+          lineHeight: 1.1,
+          fontSize: 50,
+        }}
+        onClick={() => {
+          traverseImageArray(1);
+        }}
+      >
+        &#8250;
+      </a>
+      <img
+        src={image}
+        style={{ height: windowSize.height, width: windowSize.width }}
+      />
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  let data = [];
+  await unsplash.search
+    .photos("hello", 1, 100, { order_by: "latest" })
+    .then(toJson)
+    .then((res) => {
+      data = res.results;
+    });
+  return { props: { data } };
 }
